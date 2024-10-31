43882849f06cd2a5e3072e202fc2cf81bc0ba9de
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-11-01 00:42:58 +1100

    fix

--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-python main.py -arch dinov2_vitb_exp -num_prototypes 600
-python main.py -arch dinov2_vits_exp -num_prototypes 600
 python main.py -arch dino_vitb16 -num_prototypes 600
 python main.py -arch dino_vitb16 -num_prototypes 1000
 python main.py -arch dino_vitb16 -num_prototypes 2000
+python main.py -arch dinov2_vitb_exp -num_prototypes 600
+python main.py -arch dinov2_vits_exp -num_prototypes 600
 
 # python main.py -arch dinov2_vitb_exp -num_prototypes 1000
 # python main.py -arch dinov2_vitb_exp -num_prototypes 2000
