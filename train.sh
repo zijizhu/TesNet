e1842216e12fe527f4ac2b018adccc2c40b9b22e
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-11-18 21:15:52 +1100

    add training

--- train.sh
+++ train.sh
@@ -2,11 +2,21 @@
 
 set -x
 
+python main.py -arch dinov2_vitb_exp -dataset Cars -num_prototypes 980
+python main.py -arch dinov2_vits_exp -dataset Cars -num_prototypes 980
+python main.py -arch dinov2_vitb_exp -dataset Cars -num_prototypes 588
+python main.py -arch dinov2_vits_exp -dataset Cars -num_prototypes 588
+
+python main.py -arch dinov2_vitb_exp -dataset Dogs -num_prototypes 600
+python main.py -arch dinov2_vits_exp -dataset Dogs -num_prototypes 600
+python main.py -arch dinov2_vitb_exp -dataset Dogs -num_prototypes 360
+python main.py -arch dinov2_vits_exp -dataset Dogs -num_prototypes 360
+
 # python main.py -arch dino_vitb16 -num_prototypes 600
 # python main.py -arch dino_vitb16 -num_prototypes 1000
 # python main.py -arch dino_vitb16 -num_prototypes 2000
-python main.py -arch dinov2_vitb_exp -num_prototypes 600
-python main.py -arch dinov2_vits_exp -num_prototypes 600
+# python main.py -arch dinov2_vitb_exp -num_prototypes 600
+# python main.py -arch dinov2_vits_exp -num_prototypes 600
 
 # python main.py -arch dinov2_vitb_exp -num_prototypes 1000
 # python main.py -arch dinov2_vitb_exp -num_prototypes 2000
